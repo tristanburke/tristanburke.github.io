0c28b73c90c6aaebda8b0db0d57031c949384cd8
Tristan Burke <tristan_burke@berkeley.edu>
2017-05-31 17:06:13 -0400

    created piano.js file, added bootstrap-slider

---
+++ js/piano.js
@@ -0,0 +1,123 @@
+window.onload=function(){
+    document.addEventListener("keydown",play);
+}
+function chColor(key){
+    key.style.backgroundColor='#d2d2d2';
+}
+function chBackColor(key){
+    key.style.backgroundColor='white';
+}
+function chColorBlackKey(key){
+    key.style.backgroundColor='#424242';
+}
+function chBackColorBlackKey(key){
+    key.style.backgroundColor='black';
+}
+var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+function play() {
+    switch(event.keyCode){
+        case 65: // C
+            sound(261.626);
+            var key = document.getElementById('c');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 87: //C#
+            sound(277.183);
+            var key = document.getElementById('cs');
+            chColorBlackKey(key);
+            setTimeout(function() { chBackColorBlackKey(key); }, 300);
+            break;
+        case 83: //D
+            sound(293.665);
+            var key = document.getElementById('d');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 69: //D#
+            sound(311.127);
+            var key = document.getElementById('ds');
+            chColorBlackKey(key);
+            setTimeout(function() { chBackColorBlackKey(key); }, 300);
+            break;
+        case 68: //E
+            sound(329.628);
+            var key = document.getElementById('e');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 70: //F
+            sound(349.228);
+            var key = document.getElementById('f');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 84: //F#
+            sound(369.994);
+            var key = document.getElementById('fs');
+            chColorBlackKey(key);
+            setTimeout(function() { chBackColorBlackKey(key); }, 300);
+            break;
+        case 71: //G
+            sound(391.995);
+            var key = document.getElementById('g');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 89: //G#
+            sound(415.305);
+            var key = document.getElementById('gs');
+            chColorBlackKey(key);
+            setTimeout(function() { chBackColorBlackKey(key); }, 300);
+            break;
+        case 72: //A
+            sound(440.000);
+            var key = document.getElementById('a');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 85: //A#
+            sound(466.164);
+            var key = document.getElementById('as');
+            chColorBlackKey(key);
+            setTimeout(function() { chBackColorBlackKey(key); }, 300);
+            break;
+        case 74: //B
+            sound(493.883);
+            var key = document.getElementById('b');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        case 75: //C
+            sound(523.251);
+            var key = document.getElementById('c1');
+            chColor(key);
+            setTimeout(function() { chBackColor(key); }, 300);
+            break;
+        default:
+            break;
+    }
+
+}
+function sound(freq) {
+    var oscillator = audioCtx.createOscillator();
+    var gainNode = audioCtx.createGain();
+
+    oscillator.connect(gainNode);
+    gainNode.connect(audioCtx.destination);
+
+    gainNode.gain.value = 0;
+    oscillator.frequency.value = freq;
+    oscillator.type = 'sine';
+
+    oscillator.start();
+    gainNode.gain.setValueAtTime(gainNode.gain.value, audioCtx.currentTime);
+    gainNode.gain.linearRampToValueAtTime(0.4, audioCtx.currentTime + 0.01);
+    setTimeout(
+        function() {
+            gainNode.gain.setValueAtTime(gainNode.gain.value, audioCtx.currentTime);
+            gainNode.gain.exponentialRampToValueAtTime(0.0001, audioCtx.currentTime + 0.03);
+        },
+        300
+    );
+}
